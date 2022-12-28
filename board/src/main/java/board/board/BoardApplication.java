61b117d34a43bdd543f119813b348c8d9522d4ce
Chae-1 <coguddlf1000@naver.com>
2022-12-28 14:59:32 +0900

    add member

--- board/src/main/java/board/board/BoardApplication.java
+++ board/src/main/java/board/board/BoardApplication.java
@@ -10,10 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class BoardApplication implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(new LoginInterceptor())
-//				.order(1)
-//				.addPathPatterns("/**")
-//				.excludePathPatterns("/css/**", "/board/{[0-9]}", "/members/**", "/login/**", "/logout", "/");
+		registry.addInterceptor(new LoginInterceptor())
+				.order(1)
+				.addPathPatterns("/**")
+				.excludePathPatterns("/css/**", "/board/{[0-9]}", "/members/**", "/login/**", "/logout", "/");
 	}
 
 
